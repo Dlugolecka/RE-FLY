bb58ff48d9e218a2c673ae6c53e88b41d58d0fea
Dlugolecka <ewelina.dlugolecka1210@gmail.com>
2023-05-08 12:45:53 +0200

    Initial Scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LogonComponent } from './logon/logon.component';
+import { DetailsComponent } from './details/details.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LogonComponent,
+    DetailsComponent,
+    SummaryComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
