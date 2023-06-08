ba7ad1ecbbfeebe794db0c44b1dc73c6459f2e98
Dlugolecka <ewelina.dlugolecka1210@gmail.com>
2023-06-08 21:27:51 +0200

    Add sign up switch

--- src/app/views/logon/logon.component.ts
+++ src/app/views/logon/logon.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-logon',
   templateUrl: './logon.component.html',
-  styleUrls: ['./logon.component.scss']
+  styleUrls: ['./logon.component.scss'],
 })
 export class LogonComponent {
+  showRegistration: boolean = false;
 
+  onShowRegistration() {
+    this.showRegistration = true;
+  }
 }
