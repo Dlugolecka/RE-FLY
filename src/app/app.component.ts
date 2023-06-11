1b1dd6045815756781a27c2a691a7e6abe95303f
Dlugolecka <ewelina.dlugolecka1210@gmail.com>
2023-06-11 20:42:41 +0200

    Set up flyService logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlyService } from './services/fly.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RE-FLY';
+  constructor(private flyServices: FlyService) {}
+  ngOnInit(): void {
+    this.flyServices.createFlyDateBase();
+  }
 }
--- src/app/services/fly.service.ts
+++ src/app/services/fly.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FlyService {
+  localstorageName: string = 'flyData';
+  constructor() {}
 
-  constructor() { }
+  createFlyDateBase() {
+    const data = null;
+    const dataBase = localStorage.getItem(this.localstorageName);
+    if (dataBase) {
+    } else {
+      localStorage.setItem(this.localstorageName, JSON.stringify(data));
+    }
+  }
+
+  saveFly(data: any) {
+    localStorage.setItem(this.localstorageName, JSON.stringify(data));
+  }
+
+  getFly(flyName: string) {
+    let dataBase = localStorage.getItem(this.localstorageName);
+    if (!dataBase) {
+      return;
+    }
+    const parsedDataBase = JSON.parse(dataBase);
+    return parsedDataBase;
+  }
+
+  deleteFly(flyName: string) {
+    localStorage.clear();
+  }
 }
