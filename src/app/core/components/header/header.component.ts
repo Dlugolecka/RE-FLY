4e9e82a895cb1967eb42b74137f05044fddba837
Dlugolecka <ewelina.dlugolecka1210@gmail.com>
2023-06-11 21:50:30 +0200

    Extend logon logic

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  constructor(private router: Router) {}
+  goToRegistration() {
+    this.router.navigate(['/logon'], {
+      queryParams: { showRegistration: true },
+    });
+  }
+  goToLogin() {
+    this.router.navigate(['/logon'], {
+      queryParams: { showRegistration: false },
+    });
+  }
 }
--- src/app/views/logon/logon.component.ts
+++ src/app/views/logon/logon.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-logon',
   templateUrl: './logon.component.html',
   styleUrls: ['./logon.component.scss'],
 })
-export class LogonComponent {
+export class LogonComponent implements OnInit {
   defaultView: boolean = true;
   showRegistration: boolean = false;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((res) => {
+      console.log(res);
+      this.showRegistration = JSON.parse(res['showRegistration']);
+    });
+  }
 
   onSwitchMode() {
     setTimeout(() => (this.showRegistration = !this.showRegistration), 100);
