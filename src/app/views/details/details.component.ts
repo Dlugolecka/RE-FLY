fc5bc474616ead5c5baad8be5f694b7ab7314d86
Dlugolecka <ewelina.dlugolecka1210@gmail.com>
2023-06-12 21:16:32 +0200

    Add select to details component

--- src/app/views/details/details.component.ts
+++ src/app/views/details/details.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  form: FormGroup<{
+    suitcase: FormControl;
+  }> = this.fb.group({
+    suitcase: this.fb.control('', Validators.required),
+  });
 
+  constructor(public fb: FormBuilder) {}
+  ngOnInit() {
+    this.form.valueChanges.subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
